13823473ddbc8b3fbfc6d8e131c8896e6950a69b
PaleVerge <fnme370679@gmail.com>
2026-01-31 21:54:52 +0800

    优化目录

--- 7.20QA.py
+++
@@ -1,4 +0,0 @@
-f=open('/media/rint/文件/newfolder/test.txt',mode='r')
-content=f.readline().strip()
-#content=f.readline()
-print(content)
--- 7.21QA.py
+++
@@ -1,7 +0,0 @@
-with open('/mnt/4FC1289514E05A01/newfolder/好东西.txt', 'r', encoding='utf-8') as f1,\
-    open('好东西2.0.txt','w', encoding='utf-8') as f2:
-        for line in f1:
-            line=line.strip()
-            line=line.replace('无名杀', '宝宝杀（乐）')
-            print(line)
-            f2.write(line + '\n')
